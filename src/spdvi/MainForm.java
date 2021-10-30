ca25122c89b52fe37d4fd10cf63a1ac1ad796b9b
Merce5 <mercealomar@paucasesnovescifp.cat>
2021-10-30 22:58:15 +0200

    Implemented some functions
    - Added gson 2.8.8 to the library classpath
    - Added a loadArtworks method who load all the artworks from the json and put it into the scrPane
    - Added getters, setters and tostring to ArtWorks.java
    - Added doubleClick to update one element.
    - Added functionality to update button
    - ShowDialog gets the values of the item selected when it's open by double click and by pressing the Updatemni
    - Implemented functionality to mniCreate
    - Implemented image labels who takes the image value and sets the icon depending of the selected item
    - First try of JsonWriter

--- src/spdvi/MainForm.java
+++ src/spdvi/MainForm.java
@@ -3,7 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package spdvi;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JList;
+import spdvi.dialogs.ShowDialog;
+import spdvi.dialogs.UpdateDialog;
 import spdvi.objects.*;
 
 /**
@@ -15,16 +35,42 @@ public class MainForm extends javax.swing.JFrame {
     /**
      * Creates new form MainForm
      */
-    private JList<Opo> lstOpos;
+    private static final java.lang.reflect.Type LIST_OF_ARTWORK_TYPE = new TypeToken<List<ArtWork>>() {}.getType();
+    
+    public ArrayList<ArtWork> artworks;
+    public JList<ArtWork> lstArtWork;
+    public boolean dobleclick = false;
+    public boolean update = false;
+    public UpdateDialog up = new UpdateDialog(this, true);
+    public DefaultListModel<ArtWork> artworksLstModel = new DefaultListModel<ArtWork>();
+    String imagesDirectory = System.getProperty("user.home") + "/AppData/Local/OpusList/images/";
+    
     public MainForm() {
         initComponents();
-        lstOpos = new JList<>();
-        scrOpos.setViewportView(lstOpos);
-        lstOpos.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+        lstArtWork = new JList<>();
+        scrArtWork.setViewportView(lstArtWork);
+        lstArtWork.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
             public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
-                lstOposValueChanged(evt);
+                lstArtworksValueChanged(evt);
             }
         });
+        
+        lstArtWork.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 2) {
+                    doubleClick();
+                }
+            }
+        });
+        loadArtworks();
+        
+        try {
+            BufferedImage image = ImageIO.read(new File("src/spdvi/icons/no_image.jpg"));
+            lblImage.setIcon(resizImageIcon(image));
+        } catch(IOException ioe) {
+            System.err.println("Error in ShowDialog");
+            System.err.println(ioe);
+        }
     }
 
     /**
@@ -36,8 +82,9 @@ public class MainForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        scrOpos = new javax.swing.JScrollPane();
+        lblTitle = new javax.swing.JLabel();
+        scrArtWork = new javax.swing.JScrollPane();
+        lblImage = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         mniCreate = new javax.swing.JMenuItem();
@@ -49,21 +96,33 @@ public class MainForm extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Opos Manager");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setMinimumSize(new java.awt.Dimension(500, 450));
+        setMinimumSize(new java.awt.Dimension(1100, 450));
         setPreferredSize(new java.awt.Dimension(817, 400));
 
-        jLabel1.setFont(new java.awt.Font("Georgia", 1, 48)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Opos manager");
+        lblTitle.setFont(new java.awt.Font("Georgia", 1, 48)); // NOI18N
+        lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitle.setText("Artwork manager");
+
+        lblImage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
         jMenu1.setText("File");
 
         mniCreate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/spdvi/icons/createIcon.jpg"))); // NOI18N
         mniCreate.setText("Create");
+        mniCreate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniCreateActionPerformed(evt);
+            }
+        });
         jMenu1.add(mniCreate);
 
         mniUpdate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/spdvi/icons/updateIcon.jpg"))); // NOI18N
         mniUpdate.setText("Update");
+        mniUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniUpdateActionPerformed(evt);
+            }
+        });
         jMenu1.add(mniUpdate);
 
         mniDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/spdvi/icons/deleteIcon.jpg"))); // NOI18N
@@ -83,30 +142,111 @@ public class MainForm extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 1064, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(39, Short.MAX_VALUE)
-                .addComponent(scrOpos, javax.swing.GroupLayout.PREFERRED_SIZE, 744, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34))
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(29, 29, 29)
+                .addComponent(scrArtWork, javax.swing.GroupLayout.PREFERRED_SIZE, 744, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(19, 19, 19))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(14, 14, 14)
-                .addComponent(jLabel1)
-                .addGap(29, 29, 29)
-                .addComponent(scrOpos, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addComponent(lblTitle)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(scrArtWork, javax.swing.GroupLayout.DEFAULT_SIZE, 259, Short.MAX_VALUE)
+                    .addComponent(lblImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void mniCreateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniCreateActionPerformed
+        ShowDialog sd = new ShowDialog(this, true);
+        sd.setVisible(true);
+        try {
+            JsonWriter writer = new JsonWriter(new FileWriter(System.getProperty("user.home") + "/AppData/Local/OpusList/data/obres1.json"));
+            
+            for(ArtWork a : artworks) {
+                writer.beginObject();
+                writer.name("registre").value(a.getRegistre());
+                writer.name("titol").value(a.getTitol());
+                writer.name("any").value(a.getAny());
+                writer.name("format").value(a.getFormat());
+                writer.name("autor").value(a.getAutor());
+                writer.name("imatge").value(a.getImatge());
+                writer.endObject();
+            }
+        } catch (IOException ioe) {
+            System.err.println("Error in mniCreateActionPerformed");
+            System.err.println(ioe);
+        }
+    }//GEN-LAST:event_mniCreateActionPerformed
+
+    private void mniUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniUpdateActionPerformed
+        update = true;
+        up.setVisible(true);
+    }//GEN-LAST:event_mniUpdateActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    private void lstOposValueChanged(javax.swing.event.ListSelectionEvent evt) {
-        jLabel1.setText("Cambiado");
+    private void lstArtworksValueChanged(javax.swing.event.ListSelectionEvent evt) {
+        try {
+            BufferedImage image = ImageIO.read(new File(imagesDirectory + lstArtWork.getSelectedValue().getImatge()));
+            lblImage.setIcon(resizImageIcon(image));
+        } catch(IOException ioe) {
+            System.err.println("Error in lstArtworksValueChanged");
+            System.err.println(ioe);
+        }
+    }
+    
+    private ImageIcon resizImageIcon(BufferedImage originalImage) {
+
+        int desiredHeight = 0;
+        int desiredWidth = 0;
+        float aspectRatio = (float) originalImage.getWidth() / originalImage.getHeight();
+        if (originalImage.getWidth() < originalImage.getHeight()) {
+            desiredWidth = Math.round(lblImage.getHeight() * aspectRatio);
+            desiredHeight = lblImage.getHeight();
+        } else {
+            desiredHeight = Math.round(lblImage.getWidth() / aspectRatio);
+            desiredWidth = lblImage.getWidth();
+        }
+
+        Image resultingImage = originalImage.getScaledInstance(desiredWidth, desiredHeight, Image.SCALE_SMOOTH);
+        BufferedImage outputImage = new BufferedImage(desiredWidth, desiredHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+
+        ImageIcon icon = new ImageIcon(outputImage);
+        return icon;
+    }
+    
+    private void doubleClick() {
+        dobleclick = true;
+        update = true;
+        ShowDialog sd = new ShowDialog(this, true);
+        sd.setVisible(true);
+    }
+    
+    private void loadArtworks() {
+        Gson gson = new Gson();
+        try {
+            JsonReader reader = new JsonReader(new FileReader(System.getProperty("user.home") + "/AppData/Local/OpusList/data/obres.json"));
+            
+            artworks = gson.fromJson(reader, LIST_OF_ARTWORK_TYPE);
+            
+            for(ArtWork a: artworks) {
+                artworksLstModel.addElement(a);
+            }
+            lstArtWork.setModel(artworksLstModel);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Error loading artworks");
+        }
     }
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -141,14 +281,15 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JLabel lblImage;
+    private javax.swing.JLabel lblTitle;
     private javax.swing.JMenuItem mniCreate;
     private javax.swing.JMenuItem mniDelete;
     private javax.swing.JMenuItem mniExit;
     private javax.swing.JMenuItem mniUpdate;
-    private javax.swing.JScrollPane scrOpos;
+    private javax.swing.JScrollPane scrArtWork;
     // End of variables declaration//GEN-END:variables
 }
